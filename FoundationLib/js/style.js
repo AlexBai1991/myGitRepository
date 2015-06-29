e3d2d90d5a8a2a78b3a05981cf051253a10073b3
白进国 <alexbai1991@gmail.com>
2015-06-29 16:26:20 +0800

    修改style基础库

--- FoundationLib/js/style.js
+++ FoundationLib/js/style.js
@@ -1,28 +1,59 @@
 (function (win, doc, flib) {
 	function hasClass (oElement, sClass) {
-		var reg = new RegExp('(^|\\s)' + sClass + '(\\s|$)');
+		var hasClass = false;
+		try {
+			hasClass = oElement.classList.contains(sClass);
+		} catch (ex) {
+			var reg = new RegExp('(^|\\s)' + sClass + '(\\s|$)');
+			hasClass = reg.test(oElement.className);
+		}
 		// return oElement.className.match(new RegExp('(^|\\s)' + sClass + '(\\s|$)'));
-		return reg.test(oElement.className);
+		return hasClass;
 	}
 	function getByClass (oParent, sClass) {
-		var allChildren = oParent.getElementsByTagName('*');
 		var matchedElements = [];
-		var reg = new RegExp('(^|\\s)'+ sClass +'(\\s|$)');
-		for (var i = 0; i < allChildren.length; i++) {
-			if (reg.test(allChildren[i].className)) {
-				matchedElements.push(allChildren[i]);
+		try {
+			matchedElements = oParent.getElementsByClassName(sClass);
+		} catch (ex) {
+			var allChildren = oParent.getElementsByTagName('*');
+			var i, len;
+			var reg = new RegExp('(^|\\s)'+ sClass +'(\\s|$)');
+			for (i = 0, len = allChildren.length; i < len; i++) {
+				if (reg.test(allChildren[i].className)) {
+					matchedElements.push(allChildren[i]);
+				}
 			}
 		}
 		return matchedElements;
 	}
 	function addClass (oElement, sClass) {
 		if (!hasClass(oElement, sClass)) {
-			oElement.className += ' ' + sClass;
+			try {
+				oElement.classList.add(sClass);
+			} catch (ex) {
+				oElement.className += ' ' + sClass;
+			}
 		}
 	}
 	function removeClass (oElement, sClass) {
 		if (hasClass(oElement, sClass)) {
-			oElement.className = oElement.className.replace(new RegExp(sClass), '');
+			try {
+				oElement.classList.remove(sClass);
+			} catch (ex) {
+				var pos = -1,
+					i,
+					len;
+				var classNames = oElement.className.split(/\s+/);
+				for (i = 0, len = classNames.length; i < len; i++) {
+					if (classNames[i] === sClass) {
+						pos = i;
+						break;
+					}
+				}
+				classNames.splice(pos, 1);
+				oElement.className = classNames.join(' ');
+			}
+			// oElement.className = oElement.className.replace(new RegExp(sClass), '');
 		}
 	}
 	function getStyle (oElement, name) {
@@ -30,6 +61,17 @@
 			return oElement.currentStyle[name];
 		} else {
 			return getComputedStyle(oElement, false)[name];
+		}
+	}
+	function convertToArray (node) {
+		try {
+			return Array.prototype.slice.call(node, 0);
+		} catch (ex) {
+			var array = [];
+			for (var i = 0; i < node.length; i++) {
+				array.push(node[i]);
+			}
+			return array;
 		}
 	}
 	flib.style = {
